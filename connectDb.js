66e2329aef40c0cbc8e6ba8f60fee640ea7b1c40
qwerty8199 <vinodvin135@gmail.com>
2024-08-01 10:43:44 +0530

    DB Connection file

---
+++ connectDb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const MONGOURL = "mongodb://localhost:27017/"
+const dbName = "db"
+
+export const connectDb = async() =>{
+
+    try{
+
+        await mongoose.connect(MONGOURL+dbName)
+        console.log("DB connected")
+
+    }catch(err){
+        console.log(`Exception - ${err}`)
+    }
+}
